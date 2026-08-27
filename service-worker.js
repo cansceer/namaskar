7c90d262fefb28922bd1d15bf03012c8b7ebecfe
Your Name <you@example.com>
2026-08-27 11:06:06 +0400

    Refine asana visuals and typography

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "namaskar-pwa-v3";
+﻿const CACHE_NAME = "namaskar-pwa-v4";
 const CORE_ASSETS = [
   "./",
   "index.html",
@@ -37,5 +37,6 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
